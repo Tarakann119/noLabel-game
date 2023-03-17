575a1cd705a319ab9b7c325b23c8865fdab5c9a4
Zimer-D <98761270+Zimer-D@users.noreply.github.com>
2023-03-17 16:14:49 +0300

    тесты должны работать

--- packages/client/src/App.test.tsx
+++ packages/client/src/App.test.tsx
@@ -1,12 +1,22 @@
+import * as ReactDOM from 'react-dom';
 import App from './App';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { store } from './Store/store';
 
-const appContent = 'Вот тут будет жить ваше приложение :)';
-
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 global.fetch = jest.fn(() => Promise.resolve({ json: () => Promise.resolve('hey') }));
 
 test('Example test', async () => {
-  render(<App />);
-  expect(screen.getByText(appContent)).toBeDefined();
+  const div = document.createElement('div');
+  render(
+    <Provider store={store}>
+      {' '}
+      // Set context
+      <App /> // Now App has access to context
+    </Provider>
+  );
+
+  ReactDOM.unmountComponentAtNode(div);
 });
