f15ca285a620d3b6953fb35cffadd3d26155ff6c
Andrey_St <inbox@andrey-st.ru>
2023-03-24 15:29:59 +0300

    uuid

--- packages/client/utils/generateId/index.tsx
+++ packages/client/utils/generateId/index.tsx
@@ -1,7 +1,7 @@
-export const uuid = () => {
+export const uuid = (length?: number) => {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   //@ts-ignore
   return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
-    (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16)
+    (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(length || 16)
   );
 };
