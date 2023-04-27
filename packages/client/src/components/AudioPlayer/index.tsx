51bd8142b9128f5fe816d0ce1db48ab73ec62a67
tmj1 <tmj1@mail.ru>
2023-04-27 11:08:49 +0300

    стили кнопки

--- packages/client/src/components/AudioPlayer/index.tsx
+++ packages/client/src/components/AudioPlayer/index.tsx
@@ -1,33 +1,44 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from 'react';
+
 import Beautiful from '../../assets/tracks/Orgrimmar.mp3';
+import randomClickSound from '@utils/randomClickSound/randomClickSound';
+import { Button } from '@components/Button';
 
 function AudioPlayer() {
   const sound = Beautiful;
-  const [audio] = useState(new Audio(sound));
+  const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
   const [isPlaying, setIsPlaying] = useState(false);
 
   useEffect(() => {
-    if (isPlaying) {
-      audio.play();
-    } else {
-      audio.pause();
+    setAudio(new Audio(sound));
+  }, []);
+
+  useEffect(() => {
+    if (audio) {
+      if (isPlaying) {
+        audio.play();
+      } else {
+        audio.pause();
+      }
     }
   }, [isPlaying, audio]);
 
   const handlePlay = () => {
     setIsPlaying(true);
   };
-
   const handlePause = () => {
     setIsPlaying(false);
   };
-
   return (
+
     <div>
-      <button onClick={handlePlay}>Play</button>
-      <button onClick={handlePause}>Pause</button>
+      <Button
+        text=''
+        onClick={isPlaying ? handlePause : handlePlay}
+        className={isPlaying ? 'play-btn' : 'pause-btn'}
+        style={{ margin: 20 }}
+      />
     </div>
   );
 }
-
 export default AudioPlayer;
