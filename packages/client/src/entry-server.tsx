3bf6f1a492a56de06c888a5da54578db16603e7b
Tamerlan Musaev <tamerlan.musaev@mail.ru>
2023-05-15 16:22:45 +0300

    save3

--- packages/client/src/entry-server.tsx
+++ packages/client/src/entry-server.tsx
@@ -4,10 +4,10 @@ import { matchPath } from 'react-router';
 import { StaticRouter } from 'react-router-dom/server';
 
 import { UserRepository, UserService } from './api/UserService';
+import { userReducer } from './components/Autification/slice';
 import { createStore } from './store/store';
 import { App } from './App';
 import { loadUser, Route, ROUTES } from './constants';
-import { userReducer } from './components/Autification/slice';
 
 export async function render(url: string, repository: UserRepository) {
   const [pathname] = url.split('?');
@@ -23,20 +23,20 @@ export async function render(url: string, repository: UserRepository) {
   //   await loader(store.dispatch);
   // }
 
-  store.dispatch(
-    userReducer.actions.setUser({
-      id: 1,
-      login: 'test',
-      password: 'test',
-      first_name: 'test',
-      second_name: 'test',
-      display_name: 'test',
-      email: 'test',
-      phone: 'test',
-      avatar: 'test',
-      score: 100,
-    })
-  );
+  // store.dispatch(
+  //   userReducer.actions.setUser({
+  //     id: 1,
+  //     login: 'test',
+  //     password: 'test',
+  //     first_name: 'test',
+  //     second_name: 'test',
+  //     display_name: 'test',
+  //     email: 'test',
+  //     phone: 'test',
+  //     avatar: 'test',
+  //     score: 100,
+  //   })
+  // );
 
   const initialState = store.getState();
 
